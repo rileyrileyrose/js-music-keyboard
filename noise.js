624df3328a1a483f8468be72170d420cce0798fb
Riley Spicer <riley.r.spicer@gmail.com>
2016-01-27 14:32:53 -0800

    working with clicks and keyboard keys, wahoo

--- noise.js
+++ noise.js
@@ -1,4 +1,7 @@
 $(document).ready( function() {
+
+  // playing the note on mouse click
+
   $('.note.c').click(function(){
     $('#cAudio').get(0).play();
   });
@@ -20,4 +23,40 @@ $(document).ready( function() {
   $('.note.b').click(function(){
     $('#bAudio').get(0).play();
   });
+
+  // playing the note on keyboard touch
+
+  $(document).keydown(function(e){
+    switch (e.which)  {
+      // user presses c
+      case 67:
+        $('#cAudio').get(0).play();
+          break;
+      // user presses d
+      case 68:
+        $('#dAudio').get(0).play();
+          break;
+      // user presses e
+      case 69:
+        $('#eAudio').get(0).play();
+          break;
+      // user presses f
+      case 70:
+        $('#fAudio').get(0).play();
+          break;
+      // user presses g
+      case 71:
+        $('#gAudio').get(0).play();
+          break;
+      // user presses a
+      case 65:
+        $('#aAudio').get(0).play();
+          break;
+      // user presses b
+      case 66:
+        $('#bAudio').get(0).play();
+          break;
+    }
+  });
+
 });
